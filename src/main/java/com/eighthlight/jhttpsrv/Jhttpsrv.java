c97cb54fcd477c8fbcd48d2335f23ad2897fb1ef
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-04 14:38:15 -0600

    Initial commit

---
+++ src/main/java/com/eighthlight/jhttpsrv/Jhttpsrv.java
@@ -0,0 +1,12 @@
+package com.eighthlight.jhttpsrv;
+
+/**
+ * Created by jason on 12/4/14.
+ */
+public class Jhttpsrv {
+
+    public static void main(String[] args) {
+        System.out.println("I will be an http server one day.");
+    }
+
+}
