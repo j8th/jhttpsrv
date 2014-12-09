0e4e16bb4045e790c704ee8d90160fba29272477
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-09 16:29:01 -0600

    Added HttpMessageParser::parseHeaders().

--- src/main/java/com/eighthlight/jhttpsrv/parsers/HttpMessageParser.java
+++ src/main/java/com/eighthlight/jhttpsrv/parsers/HttpMessageParser.java
@@ -11,17 +11,30 @@ import java.util.Map;
  */
 public class HttpMessageParser {
 
-    public Map<String, String> parseRequestLine(String myRequestLine) {
+    public static final String CRLF = "\r\n";
+
+    public Map<String, String> parseRequestLine(String requestLine) {
         HashMap<String, String> result = new HashMap<String, String>();
-        String[] tokens = myRequestLine.trim().split(" ");
+        String[] tokens = requestLine.trim().split(" ");
         result.put("Method", tokens[0]);
         result.put("URL", tokens[1]);
         result.put("Protocol-Version", tokens[2].substring(tokens[2].indexOf('/')+1));
         return result;
     }
 
-    public Map<String, String> parseHeaders(String myHeaderString) {
-        return new HashMap<String, String>();
+    public Map<String, String> parseHeaders(String headers) {
+        HashMap<String, String> result = new HashMap<String, String>();
+        String[] headerLines = headers.split(CRLF);
+        String[] tokens;
+        String key;
+        String val;
+        for(int i=0; i < headerLines.length; i++) {
+            tokens = headerLines[i].split(":");
+            key = tokens[0];
+            val = tokens[1].trim();
+            result.put(key, val);
+        }
+        return result;
     }
 
     public HttpMessage parseInputStream(InputStream myis) {
--- src/test/java/com/eighthlight/jhttpsrv/parsers/HttpMessageParserTest.java
+++ src/test/java/com/eighthlight/jhttpsrv/parsers/HttpMessageParserTest.java
@@ -33,4 +33,20 @@ public class HttpMessageParserTest {
 
         Assert.assertEquals(expected, myMap);
     }
+
+    @Test
+    public void parseHeaders() {
+        Map<String, String> expected = new HashMap<String, String>();
+        expected.put("Host", "localhost");
+        expected.put("Connection", "keep-alive");
+        expected.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        expected.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36");
+        expected.put("Accept-Encoding", "gzip, deflate, sdch");
+        expected.put("Accept-Language", "en-US,en;q=0.8");
+
+        Map<String, String> myMap = myParser.parseHeaders(GETRequestChrome.Headers);
+
+        Assert.assertEquals(expected, myMap);
+
+    }
 }
