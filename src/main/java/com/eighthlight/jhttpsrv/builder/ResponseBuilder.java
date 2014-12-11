82746b08962370b9b661203d969c3724044dca13
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-11 16:43:04 -0600

    ResponseBuilder.buildStatusLine().

--- src/main/java/com/eighthlight/jhttpsrv/builder/ResponseBuilder.java
+++ src/main/java/com/eighthlight/jhttpsrv/builder/ResponseBuilder.java
@@ -6,6 +6,15 @@ import com.eighthlight.jhttpsrv.response.Response;
  * Created by jason on 12/10/14.
  */
 public class ResponseBuilder {
+    public String buildStatusLine(Response response) {
+        String result = String.format("HTTP/%s %s %s\r\n",
+                response.getProtocolVersion(),
+                response.getStatusCode(),
+                response.getReasonPhrase());
+
+        return result;
+    }
+
     public byte[] buildResponse(Response response) {
         return new byte[0];
     }
--- src/test/java/com/eighthlight/jhttpsrv/builder/ResponseBuilderTest.java
+++ src/test/java/com/eighthlight/jhttpsrv/builder/ResponseBuilderTest.java
@@ -1,21 +1,35 @@
 package com.eighthlight.jhttpsrv.builder;
 
+import com.eighthlight.jhttpsrv.response.Response;
+import com.eighthlight.jhttpsrv.shared.StatusCodes;
+import com.eighthlight.jhttpsrv.testmessage.chrome.GETResponseChrome;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ResponseBuilderTest {
+    private Response response;
+    private ResponseBuilder responseBuilder;
 
     @Before
     public void setUp() throws Exception {
+        response = new Response();
+        responseBuilder = new ResponseBuilder();
 
+        response.setStatusCode(StatusCodes.OK);
     }
 
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void buildStatusLine() {
+        Assert.assertEquals(GETResponseChrome.STATUS_LINE, responseBuilder.buildStatusLine(response));
     }
 
     @Test
