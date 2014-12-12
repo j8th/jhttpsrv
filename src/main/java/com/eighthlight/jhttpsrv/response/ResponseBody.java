609fd5f36f7a438b7ba92918078ba52bb73b1696
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-12 09:45:54 -0600

    ResponseBody.setContent().

--- src/main/java/com/eighthlight/jhttpsrv/response/ResponseBody.java
+++ src/main/java/com/eighthlight/jhttpsrv/response/ResponseBody.java
@@ -4,4 +4,13 @@ package com.eighthlight.jhttpsrv.response;
  * Created by jason on 12/11/14.
  */
 public class ResponseBody {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String myContent) {
+        content = myContent;
+    }
 }
--- src/test/java/com/eighthlight/jhttpsrv/response/ResponseBodyTest.java
+++ src/test/java/com/eighthlight/jhttpsrv/response/ResponseBodyTest.java
@@ -1,19 +1,29 @@
 package com.eighthlight.jhttpsrv.response;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ResponseBodyTest {
+    private ResponseBody body;
 
     @Before
     public void setUp() throws Exception {
-
+        body = new ResponseBody();
     }
 
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void set_get_Content() {
+        Assert.assertNull(body.getContent());
+        body.setContent("<p>hello</p>\n");
+        Assert.assertEquals("<p>hello</p>\n", body.getContent());
     }
 }
