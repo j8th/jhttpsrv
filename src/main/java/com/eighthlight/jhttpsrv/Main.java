d40989b3eb13e3140988d0af3d1ae6dd792d10b2
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-16 15:57:15 -0600

    Refactored Main to be a little more testable.

--- src/main/java/com/eighthlight/jhttpsrv/Main.java
+++ src/main/java/com/eighthlight/jhttpsrv/Main.java
@@ -1,6 +1,10 @@
 package com.eighthlight.jhttpsrv;
 
+import com.eighthlight.jhttpsrv.handler.HelloWorldRequestHandler;
+import com.eighthlight.jhttpsrv.handler.TimeRequestHandler;
+import com.eighthlight.jhttpsrv.router.Router;
 import com.eighthlight.jhttpsrv.server.Jhttpsrv;
+import com.eighthlight.jhttpsrv.shared.ProtocolStrings;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -9,11 +13,31 @@ import java.net.ServerSocket;
  * Created by jason on 12/4/14.
  */
 public class Main {
+    private static ServerSocket serverSocket;
+    private static Router router;
+    private static Jhttpsrv jhttpsrv;
+
+    public static void init(ServerSocket myServerSocket) throws IOException {
+        serverSocket = myServerSocket;
+        router = new Router();
+
+        // Setup the routes.
+        router.addRoute(ProtocolStrings.HTTP_METHOD_GET, "/time", TimeRequestHandler.class);
+
+        jhttpsrv = new Jhttpsrv(serverSocket, router);
+    }
+
+    public static Jhttpsrv getJhttpsrv() {
+        return jhttpsrv;
+    }
+
+
 
     public static void main(String[] args) throws IOException {
         ServerSocket myServerSocket = new ServerSocket(80);
-        Jhttpsrv server = new Jhttpsrv(myServerSocket);
-        server.run();
+        init(myServerSocket);
+
+        jhttpsrv.run();
     }
 
 }
