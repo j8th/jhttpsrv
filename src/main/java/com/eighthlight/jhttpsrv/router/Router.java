0ac5edf009516b96447d6162511e16ef4d9a6ab5
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-16 12:00:45 -0600

    Router.route() and Router.addRoute().

--- src/main/java/com/eighthlight/jhttpsrv/router/Router.java
+++ src/main/java/com/eighthlight/jhttpsrv/router/Router.java
@@ -1,7 +1,45 @@
 package com.eighthlight.jhttpsrv.router;
 
+import com.eighthlight.jhttpsrv.handler.OKRequestHandler;
+import com.eighthlight.jhttpsrv.handler.RequestHandler;
+import com.eighthlight.jhttpsrv.request.Request;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by jason on 12/10/14.
  */
 public class Router {
+    private Class<? extends RequestHandler> defaultHandler;
+    private Map<String, Class> routes;
+
+    public Router() {
+        defaultHandler = OKRequestHandler.class;
+        routes = new LinkedHashMap<String, Class>();
+    }
+
+    public RequestHandler route(Request request) {
+        String requestRoute = String.format("%s %s", request.getMethod(), request.getURL());
+        Class<? extends RequestHandler> handlerClass = routes.get(requestRoute);
+
+        RequestHandler handler = null;
+        try {
+            if(handlerClass == null)
+                handler = defaultHandler.newInstance();
+            else
+                handler = handlerClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return handler;
+    }
+
+    public void addRoute(String httpmethod, String url, Class<? extends RequestHandler> handlerClass) {
+        String route = String.format("%s %s", httpmethod, url);
+        routes.put(route, handlerClass);
+    }
 }
--- src/test/java/com/eighthlight/jhttpsrv/router/RouterTest.java
+++ src/test/java/com/eighthlight/jhttpsrv/router/RouterTest.java
@@ -1,19 +1,60 @@
 package com.eighthlight.jhttpsrv.router;
 
+import com.eighthlight.jhttpsrv.handler.HelloWorldRequestHandler;
+import com.eighthlight.jhttpsrv.handler.OKRequestHandler;
+import com.eighthlight.jhttpsrv.handler.RequestHandler;
+import com.eighthlight.jhttpsrv.parser.RequestParser;
+import com.eighthlight.jhttpsrv.request.Request;
+import com.eighthlight.jhttpsrv.response.Response;
+import com.eighthlight.jhttpsrv.shared.ProtocolStrings;
+import com.eighthlight.jhttpsrv.shared.StatusCodes;
+import com.eighthlight.jhttpsrv.testmessage.chrome.GETRequestChrome;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
 public class RouterTest {
+    private Router router;
+    private Request request;
 
     @Before
     public void setUp() throws Exception {
+        RequestParser parser = new RequestParser();
+        InputStream is = new ByteArrayInputStream(GETRequestChrome.ENTIRE_MESSAGE.getBytes(StandardCharsets.UTF_8));
 
+        router = new Router();
+        request = parser.parseInputStream(is);
     }
 
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void testDefaultRoute() throws Exception {
+        // A new Router with no routes established should return the default default handler, the OKRequestHandler.
+        RequestHandler handler = router.route(request);
+        Assert.assertThat(handler, instanceOf(OKRequestHandler.class));
+    }
+
+    @Test
+    public void route() throws Exception {
+
+    }
+
+    @Test
+    public void addRoute() throws Exception {
+        router.addRoute(ProtocolStrings.HTTP_METHOD_GET, "/helloworld", HelloWorldRequestHandler.class);
+        RequestHandler handler = router.route(request);
+        Assert.assertThat(handler, instanceOf(HelloWorldRequestHandler.class));
     }
 }
