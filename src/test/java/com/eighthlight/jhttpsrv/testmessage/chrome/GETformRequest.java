3d85e73b6a4d8c3eb238b9e7a51a9ff8e35b56a7
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-18 14:23:30 -0600

    Deleted the FormRequestHandler. Added form.html

--- src/test/java/com/eighthlight/jhttpsrv/testmessage/chrome/GETformRequest.java
+++ src/test/java/com/eighthlight/jhttpsrv/testmessage/chrome/GETformRequest.java
@@ -5,7 +5,7 @@ package com.eighthlight.jhttpsrv.testmessage.chrome;
  */
 public class GETformRequest {
 
-    public final static String REQUEST_LINE = "GET /form HTTP/1.1\r\n";
+    public final static String REQUEST_LINE = "GET /form.html HTTP/1.1\r\n";
 
     public final static String HEADERS =
                     "Host: localhost\r\n" +
