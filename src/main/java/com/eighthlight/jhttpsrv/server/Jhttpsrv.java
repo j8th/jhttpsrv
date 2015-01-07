ec7a8ef16db5bb6dae512d2c13a3634522816391
Jason <jason@Jasons-MacBook-Pro.local>
2015-01-07 15:42:12 -0600

    Removed unused import statements.

--- src/main/java/com/eighthlight/jhttpsrv/server/Jhttpsrv.java
+++ src/main/java/com/eighthlight/jhttpsrv/server/Jhttpsrv.java
@@ -2,15 +2,10 @@ package com.eighthlight.jhttpsrv.server;
 
 import com.eighthlight.jhttpsrv.builder.ResponseBuilder;
 import com.eighthlight.jhttpsrv.parser.RequestParser;
-import com.eighthlight.jhttpsrv.request.Request;
-import com.eighthlight.jhttpsrv.response.Response;
-import com.eighthlight.jhttpsrv.handler.RequestHandler;
 import com.eighthlight.jhttpsrv.router.Router;
 import com.eighthlight.jhttpsrv.worker.Worker;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
