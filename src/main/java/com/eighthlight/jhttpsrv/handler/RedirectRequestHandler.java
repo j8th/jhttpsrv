669c1c3932fadb0396bc632faff1b9fa55f4f6bf
Jason <jason@Jasons-MacBook-Pro.local>
2015-01-13 14:47:10 -0600

    RedirectRequestHandler takes a string indicating the redirect location. Set /redirect => / for cobspec.

--- src/main/java/com/eighthlight/jhttpsrv/handler/RedirectRequestHandler.java
+++ src/main/java/com/eighthlight/jhttpsrv/handler/RedirectRequestHandler.java
@@ -6,12 +6,18 @@ import com.eighthlight.jhttpsrv.response.ResponseHeader;
 import com.eighthlight.jhttpsrv.constants.StatusCodes;
 
 public class RedirectRequestHandler implements RequestHandler {
+    private String locationURL;
+
+    public RedirectRequestHandler(String locationURL) {
+        this.locationURL = locationURL;
+    }
+
     public Response run(Request request) {
         Response response = new Response();
         ResponseHeader header = new ResponseHeader();
 
         response.setStatusCode(StatusCodes.TEMPORARY_REDIRECT);
-        header.setLocation("/time");
+        header.setLocation(locationURL);
         response.setHeaders(header);
 
         return response;
