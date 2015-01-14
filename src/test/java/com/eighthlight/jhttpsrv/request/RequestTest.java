4d49af7b29b6cfb282125fc2c376a0ad9613d962
Jason <jason@Jasons-MacBook-Pro.local>
2015-01-14 13:13:58 -0600

    Static import Assert.* in RequestTest.

--- src/test/java/com/eighthlight/jhttpsrv/request/RequestTest.java
+++ src/test/java/com/eighthlight/jhttpsrv/request/RequestTest.java
@@ -6,7 +6,7 @@ import com.eighthlight.jhttpsrv.testmessage.chrome.GETHelloworldRequest;
 import com.eighthlight.jhttpsrv.testmessage.chrome.POSTFormRequest;
 import com.eighthlight.jhttpsrv.testmessage.chrome.PUTtextfileRequest;
 import com.eighthlight.jhttpsrv.testmessage.chrome.TestRequestMaker;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,47 +32,57 @@ public class RequestTest {
 
     @Test
     public void testGetHeaders() {
-        Assert.assertEquals(header, request.getHeader());
+        assertEquals(header, request.getHeader());
     }
 
     @Test
     public void testGetBody() {
-        Assert.assertEquals(body, request.getBody());
+        assertEquals(body, request.getBody());
     }
 
     @Test
     public void testGetURL() {
-        Assert.assertEquals("/helloworld", request.getURL());
+        assertEquals("/helloworld", request.getURL());
+    }
+
+    @Test
+    public void testGetURLPathPart() {
+        
+    }
+
+    @Test
+    public void testGetURLQueryPart() {
+
     }
 
     @Test
     public void testGetMethod() {
-        Assert.assertEquals(ProtocolStrings.HTTP_METHOD_GET, request.getMethod());
+        assertEquals(ProtocolStrings.HTTP_METHOD_GET, request.getMethod());
     }
 
     @Test
     public void testGETRequestIsValid() {
-        Assert.assertTrue(request.isValid());
+        assertTrue(request.isValid());
     }
 
     @Test
     public void testPOSTRequestIsValid() throws Exception {
         Request request = TestRequestMaker.fromString(POSTFormRequest.ENTIRE_MESSAGE);
 
-        Assert.assertTrue(request.isValid());
+        assertTrue(request.isValid());
     }
 
     @Test
     public void testPUTRequestIsValid() throws Exception {
         Request request = TestRequestMaker.fromString(PUTtextfileRequest.ENTIRE_MESSAGE);
 
-        Assert.assertTrue(request.isValid());
+        assertTrue(request.isValid());
     }
 
     @Test
     public void testEmptyRequestIsInvalid() {
         request = new Request(new HashMap<String, String>(), new RequestHeader(new HashMap<String, String>()), new RequestBody(""));
-        Assert.assertFalse(request.isValid());
+        assertFalse(request.isValid());
     }
 
     @Test
@@ -83,6 +93,6 @@ public class RequestTest {
         requestLine.put(ProtocolStrings.PROTOCOL_VERSION, "HTTP/1.1");
 
         request = new Request(requestLine, new RequestHeader(new HashMap<String, String>()), new RequestBody(""));
-        Assert.assertFalse(request.isValid());
+        assertFalse(request.isValid());
     }
 }
