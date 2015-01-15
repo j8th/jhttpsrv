0d968be2a04c7fb6c3e94695ea601de1de13d507
Jason <jason@Jasons-MacBook-Pro.local>
2015-01-15 13:54:34 -0600

    Cleaned up some code.

--- src/main/java/com/eighthlight/jhttpsrv/request/RequestHeader.java
+++ src/main/java/com/eighthlight/jhttpsrv/request/RequestHeader.java
@@ -5,32 +5,20 @@ import com.eighthlight.jhttpsrv.constants.ProtocolStrings;
 import java.util.Map;
 
 public class RequestHeader {
-    private String host;
-    private String connection;
-    private int contentlength;
     private String[] accept;
-    private String userAgent;
     private String[] acceptEncoding;
     private String[] acceptLanguage;
+    private String connection;
+    private int contentlength;
+    private String host;
     private String range;
+    private String userAgent;
 
     public RequestHeader(Map<String, String> headers) {
         String myHeader;
 
-        myHeader = headers.get(ProtocolStrings.HOST);
-        host = myHeader == null ? "" : myHeader;
-
-        myHeader = headers.get(ProtocolStrings.CONNECTION);
-        connection = myHeader == null ? "" : myHeader;
-
-        myHeader = headers.get(ProtocolStrings.CONTENT_LENGTH);
-        contentlength = myHeader == null ? 0 : Integer.parseInt(myHeader);
-
         myHeader = headers.get(ProtocolStrings.ACCEPT);
         accept = myHeader == null ? new String[0] : myHeader.split(",");
-
-        myHeader = headers.get(ProtocolStrings.USER_AGENT);
-        userAgent = myHeader == null ? "" : myHeader;
 
         myHeader = headers.get(ProtocolStrings.ACCEPT_ENCODING);
         acceptEncoding = myHeader == null ? new String[0] : myHeader.split(", ");
@@ -38,24 +26,24 @@ public class RequestHeader {
         myHeader = headers.get(ProtocolStrings.ACCEPT_LANGUAGE);
         acceptLanguage = myHeader == null ? new String[0] : myHeader.split(",");
 
+        myHeader = headers.get(ProtocolStrings.CONNECTION);
+        connection = myHeader == null ? "" : myHeader;
+
+        myHeader = headers.get(ProtocolStrings.CONTENT_LENGTH);
+        contentlength = myHeader == null ? 0 : Integer.parseInt(myHeader);
+
+        myHeader = headers.get(ProtocolStrings.HOST);
+        host = myHeader == null ? "" : myHeader;
+
         myHeader = headers.get(ProtocolStrings.RANGE);
         range = myHeader == null ? "" : myHeader;
-    }
 
-    public String getHost() {
-        return host;
-    }
-
-    public String getConnection() {
-        return connection;
+        myHeader = headers.get(ProtocolStrings.USER_AGENT);
+        userAgent = myHeader == null ? "" : myHeader;
     }
 
     public String[] getAccept() {
         return accept;
-    }
-
-    public String getUserAgent() {
-        return userAgent;
     }
 
     public String[] getAcceptEncoding() {
@@ -66,12 +54,24 @@ public class RequestHeader {
         return acceptLanguage;
     }
 
+    public String getConnection() {
+        return connection;
+    }
+
     public int getContentLength() {
         return contentlength;
     }
 
+    public String getHost() {
+        return host;
+    }
+
     public String getRange() {
         return range;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
     }
 
     public boolean isEmpty() {
