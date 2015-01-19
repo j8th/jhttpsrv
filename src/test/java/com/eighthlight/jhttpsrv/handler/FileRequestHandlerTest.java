5271f3afb3f37af0db8c1e2397e9359b768d5dba
Jason <jason@Jasons-MacBook-Pro.local>
2015-01-19 14:24:10 -0600

    Static import Assert.*.

--- src/test/java/com/eighthlight/jhttpsrv/handler/FileRequestHandlerTest.java
+++ src/test/java/com/eighthlight/jhttpsrv/handler/FileRequestHandlerTest.java
@@ -6,7 +6,7 @@ import com.eighthlight.jhttpsrv.response.Response;
 import com.eighthlight.jhttpsrv.constants.MIMETypes;
 import com.eighthlight.jhttpsrv.constants.StatusCodes;
 import com.eighthlight.jhttpsrv.testmessage.chrome.*;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,7 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static java.nio.charset.StandardCharsets.*;
 import static org.hamcrest.CoreMatchers.containsString;
 
 public class FileRequestHandlerTest {
@@ -36,8 +35,8 @@ public class FileRequestHandlerTest {
         String FilePath = FileRequestHandler.getRootDir() + "/index.html";
         byte[] bytes = Files.readAllBytes(Paths.get(FilePath));
 
-        Assert.assertEquals(MIMETypes.HTML, response.getHeaders().getContentType());
-        Assert.assertArrayEquals(bytes, response.getBody().getContent());
+        assertEquals(MIMETypes.HTML, response.getHeaders().getContentType());
+        assertArrayEquals(bytes, response.getBody().getContent());
     }
 
     @Test
@@ -45,7 +44,7 @@ public class FileRequestHandlerTest {
         Request request = TestRequestMaker.fromString(GETtextfileRequest.ENTIRE_MESSAGE);
         Response response = fileRequestHandler.run(request);
 
-        Assert.assertEquals(MIMETypes.PLAIN_TEXT, response.getHeaders().getContentType());
+        assertEquals(MIMETypes.PLAIN_TEXT, response.getHeaders().getContentType());
     }
 
     @Test
@@ -54,8 +53,8 @@ public class FileRequestHandlerTest {
         Response response = fileRequestHandler.run(request);
         byte[] fileBytes = getTestFileBytes("/images/testimg.jpeg");
 
-        Assert.assertEquals(MIMETypes.JPEG, response.getHeaders().getContentType());
-        Assert.assertArrayEquals(fileBytes, response.getBody().getContent());
+        assertEquals(MIMETypes.JPEG, response.getHeaders().getContentType());
+        assertArrayEquals(fileBytes, response.getBody().getContent());
     }
 
     @Test
@@ -64,9 +63,9 @@ public class FileRequestHandlerTest {
         Response response = fileRequestHandler.run(request);
         byte[] fileBytes = getTestFileBytes("/download/testdl.txt.gz");
 
-        Assert.assertEquals(MIMETypes.GZIP, response.getHeaders().getContentType());
-        Assert.assertArrayEquals(fileBytes, response.getBody().getContent());
-        Assert.assertEquals(StatusCodes.OK, response.getStatusCode());
+        assertEquals(MIMETypes.GZIP, response.getHeaders().getContentType());
+        assertArrayEquals(fileBytes, response.getBody().getContent());
+        assertEquals(StatusCodes.OK, response.getStatusCode());
     }
 
     @Test
@@ -75,9 +74,9 @@ public class FileRequestHandlerTest {
         Response response = fileRequestHandler.run(request);
         byte[] fileBytes = getTestFileBytes("/test.json");
 
-        Assert.assertEquals(MIMETypes.JSON, response.getHeaders().getContentType());
-        Assert.assertArrayEquals(fileBytes, response.getBody().getContent());
-        Assert.assertEquals(StatusCodes.OK, response.getStatusCode());
+        assertEquals(MIMETypes.JSON, response.getHeaders().getContentType());
+        assertArrayEquals(fileBytes, response.getBody().getContent());
+        assertEquals(StatusCodes.OK, response.getStatusCode());
     }
 
     @Test
@@ -85,12 +84,12 @@ public class FileRequestHandlerTest {
         Request request = TestRequestMaker.fromString(GETDirectoryRequest.ENTIRE_MESSAGE);
         Response response = fileRequestHandler.run(request);
 
-        Assert.assertEquals(StatusCodes.OK, response.getStatusCode());
-        Assert.assertEquals(MIMETypes.HTML, response.getHeaders().getContentType());
+        assertEquals(StatusCodes.OK, response.getStatusCode());
+        assertEquals(MIMETypes.HTML, response.getHeaders().getContentType());
         String content = new String(response.getBody().getContent(), StandardCharsets.UTF_8);
-        Assert.assertThat(content, containsString("<a href=\"/dirtest/candy/\">candy/</a>"));
-        Assert.assertThat(content, containsString("<a href=\"/dirtest/hello.html\">hello.html</a>"));
-        Assert.assertThat(content, containsString("<a href=\"/\">^</a>"));
+        assertThat(content, containsString("<a href=\"/dirtest/candy/\">candy/</a>"));
+        assertThat(content, containsString("<a href=\"/dirtest/hello.html\">hello.html</a>"));
+        assertThat(content, containsString("<a href=\"/\">^</a>"));
     }
 
     @Test
@@ -98,7 +97,7 @@ public class FileRequestHandlerTest {
         Request request = TestRequestMaker.fromString(GET404Request.ENTIRE_MESSAGE);
         Response response = fileRequestHandler.run(request);
 
-        Assert.assertEquals(StatusCodes.NOT_FOUND, response.getStatusCode());
+        assertEquals(StatusCodes.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
@@ -106,7 +105,7 @@ public class FileRequestHandlerTest {
         Request request = TestRequestMaker.fromString(POSTtextfileRequest.ENTIRE_MESSAGE);
         Response response = fileRequestHandler.run(request);
 
-        Assert.assertEquals(StatusCodes.METHOD_NOT_ALLOWED, response.getStatusCode());
+        assertEquals(StatusCodes.METHOD_NOT_ALLOWED, response.getStatusCode());
     }
 
     @Test
@@ -114,20 +113,20 @@ public class FileRequestHandlerTest {
         Request request = TestRequestMaker.fromString(PUTtextfileRequest.ENTIRE_MESSAGE);
         Response response = fileRequestHandler.run(request);
 
-        Assert.assertEquals(ProtocolStrings.HTTP_METHOD_PUT, request.getMethod());
-        Assert.assertEquals(StatusCodes.METHOD_NOT_ALLOWED, response.getStatusCode());
+        assertEquals(ProtocolStrings.HTTP_METHOD_PUT, request.getMethod());
+        assertEquals(StatusCodes.METHOD_NOT_ALLOWED, response.getStatusCode());
     }
 
     @Test
     public void testDefaultRootDirectory() throws Exception {
-        Assert.assertEquals(System.getProperty("user.dir") + "/www", FileRequestHandler.getRootDir());
+        assertEquals(System.getProperty("user.dir") + "/www", FileRequestHandler.getRootDir());
     }
 
     @Test
     public void testSetRootDirectory() throws Exception {
         String subDir = defaultRootDir + "/dirtest";
         FileRequestHandler.setRootDir(subDir);
-        Assert.assertEquals(subDir, FileRequestHandler.getRootDir());
+        assertEquals(subDir, FileRequestHandler.getRootDir());
     }
 
     @Test(expected = IllegalArgumentException.class)
