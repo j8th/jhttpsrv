5b781c11c2a0653b83d0d621dd52299c56e0b492
Jason <jason@Jasons-MacBook-Pro.local>
2015-01-20 16:53:48 -0600

    Static import Assert.

--- src/test/java/com/eighthlight/jhttpsrv/constants/MIMETypesTest.java
+++ src/test/java/com/eighthlight/jhttpsrv/constants/MIMETypesTest.java
@@ -1,31 +1,31 @@
 package com.eighthlight.jhttpsrv.constants;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MIMETypesTest {
     @Test
     public void testFileExt2MIMEType() throws Exception {
-        Assert.assertEquals(MIMETypes.HTML, MIMETypes.FileExt2MIMEType("html"));
-        Assert.assertEquals(MIMETypes.PLAIN_TEXT, MIMETypes.FileExt2MIMEType("txt"));
-        Assert.assertEquals(MIMETypes.JPEG, MIMETypes.FileExt2MIMEType("jpg"));
-        Assert.assertEquals(MIMETypes.GZIP, MIMETypes.FileExt2MIMEType("gz"));
-        Assert.assertEquals(MIMETypes.JSON, MIMETypes.FileExt2MIMEType("json"));
+        assertEquals(MIMETypes.HTML, MIMETypes.FileExt2MIMEType("html"));
+        assertEquals(MIMETypes.PLAIN_TEXT, MIMETypes.FileExt2MIMEType("txt"));
+        assertEquals(MIMETypes.JPEG, MIMETypes.FileExt2MIMEType("jpg"));
+        assertEquals(MIMETypes.GZIP, MIMETypes.FileExt2MIMEType("gz"));
+        assertEquals(MIMETypes.JSON, MIMETypes.FileExt2MIMEType("json"));
     }
 
     @Test
     public void testFileExt2MIMETypeWithLeadingDot() throws Exception {
-        Assert.assertEquals(MIMETypes.HTML, MIMETypes.FileExt2MIMEType(".html"));
-        Assert.assertEquals(MIMETypes.PLAIN_TEXT, MIMETypes.FileExt2MIMEType(".txt"));
-        Assert.assertEquals(MIMETypes.JPEG, MIMETypes.FileExt2MIMEType(".jpg"));
-        Assert.assertEquals(MIMETypes.GZIP, MIMETypes.FileExt2MIMEType(".gz"));
-        Assert.assertEquals(MIMETypes.JSON, MIMETypes.FileExt2MIMEType(".json"));
+        assertEquals(MIMETypes.HTML, MIMETypes.FileExt2MIMEType(".html"));
+        assertEquals(MIMETypes.PLAIN_TEXT, MIMETypes.FileExt2MIMEType(".txt"));
+        assertEquals(MIMETypes.JPEG, MIMETypes.FileExt2MIMEType(".jpg"));
+        assertEquals(MIMETypes.GZIP, MIMETypes.FileExt2MIMEType(".gz"));
+        assertEquals(MIMETypes.JSON, MIMETypes.FileExt2MIMEType(".json"));
     }
 
     @Test
     public void testInvalidAndUnknownFileExtensions() {
-        Assert.assertEquals(MIMETypes.OCTET_STREAM, MIMETypes.FileExt2MIMEType(".some-insane-extension"));
-        Assert.assertEquals(MIMETypes.OCTET_STREAM, MIMETypes.FileExt2MIMEType("."));
-        Assert.assertEquals(MIMETypes.OCTET_STREAM, MIMETypes.FileExt2MIMEType(""));
+        assertEquals(MIMETypes.OCTET_STREAM, MIMETypes.FileExt2MIMEType(".some-insane-extension"));
+        assertEquals(MIMETypes.OCTET_STREAM, MIMETypes.FileExt2MIMEType("."));
+        assertEquals(MIMETypes.OCTET_STREAM, MIMETypes.FileExt2MIMEType(""));
     }
 }
